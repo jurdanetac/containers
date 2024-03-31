47d3df06891cc54710536d5573d4287529cbc7c1
Juan Urdaneta <jurdanetac10@gmail.com>
2024-03-31 10:48:02 -0400

    Set up backend enviroment using container

--- todo-app/todo-backend/mongo/index.js
+++ todo-app/todo-backend/mongo/index.js
@@ -3,10 +3,17 @@ const Todo = require("./models/Todo");
 const { MONGO_URL } = require("../util/config");
 
 if (MONGO_URL && !mongoose.connection.readyState)
-  mongoose.connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  mongoose
+    .connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Mongoose server has started");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 
 module.exports = {
   Todo,
